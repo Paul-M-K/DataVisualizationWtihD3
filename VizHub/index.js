7b1df32c708a0fd7bc9282dde99b8ba2d0c541b5
PaulK <paul.kot@withyouwithme.com>
2022-12-07 10:27:26 -0800

    Create Bar Chart

--- VizHub/index.js
+++ VizHub/index.js
@@ -4,18 +4,43 @@ const width = +svg.attr('width');
 const height = +svg.attr('height');
 
 const render = data => {
-    const xScale = d3.scaleLinear();
+    const xValue = d => d.count;
+    const yValue = d => d.jobtitle;
+    const margin = { top:20, right: 40, bottom:20, left:100};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0,innerWidth]);
+
+    const yScale = d3.scaleBand()
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.1);
+
+    const g = svg.append('g')
+        .attr('transform',`translate(${margin.left},${margin.top})`);
     
-    svg.selectAll('rect').data(data)
+    g.append('g').call(d3.axisLeft(yScale));
+    g.append('g').call(d3.axisBottom(xScale))
+    .attr('transform',`translate(0,${innerHeight})`);
+    
+    g.selectAll('rect').data(data)
         .enter().append('rect')
-            .attr('width', 300)
-            .attr('height', 300);
+            .attr('y', d => yScale(yValue(d)))
+            .attr('width', d => xScale(xValue(d)))
+            .attr('height', yScale.bandwidth());
 };
 
-d3.csv('data.csv').then(data => {
-    data.forEach(d => {
-        d.population = +d.population * 1000;
+d3.csv('data.csv')
+    .then(data => {
+    
+        data.forEach(d => {
+        d.count = +d.count;
     })
+
+    console.log(data);
     render(data);
 });
 
